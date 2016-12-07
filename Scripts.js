09fa54c823ae8f8d2d335c0f937ed99e4a45b28b
ANTHONYYPC\Anthony <Lemleyac@gmail.com>
2016-12-06 20:32:49 -0500

    the walking dead is a decent show

--- Scripts.js
+++ Scripts.js
@@ -35,4 +35,44 @@ function Deck() {
             array.push(new Card(i, j));
         }
     }
+    array.sort(function() {return 0.5 - Math.random()});
+
+
+    this.deal = function()
+    {
+        var card = array[0];
+        array.splice(0,1);
+        return card;
+    };
 }
+
+
+function Hand(deck) {
+
+    var array =[];
+
+    this.deck = deck;
+    array.push(this.deck.deal());
+    array.push(this.deck.deal());
+
+    this.getHand=function()
+    {
+        return array;
+    };
+
+
+    this.score = function() {
+        var sum =0;
+        var num1s = 0;
+        for(var i = 0 ; i < array.length; i++)
+        {
+            sum+=array[i].getValue();
+            if(array[i].getValue() ===11)   num1s++;
+        }
+
+        while(sum>21 && num1s>0)
+        {
+            sum-=10;num1s--;
+        }
+        return sum;
+    }
